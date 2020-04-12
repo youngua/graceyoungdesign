edd086e829e27a841291053762a318e7651af91e
youngua <gyoung@risd.edu>
2020-04-12 09:27:51 -0400

    added scroll to top button

--- script/jquery.js
+++ script/jquery.js
@@ -1,14 +1,17 @@
-jQuery("document").ready(function($) {
+/**
+ * This animates the scramble of the navigation text
+ */
+jQuery("document").ready(function ($) {
   // Set effect velocity in ms
   var velocity = 100;
 
   var shuffleElement = $(".shuffle");
 
-  $.each(shuffleElement, function(index, item) {
+  $.each(shuffleElement, function (index, item) {
     $(item).attr("data-text", $(item).text());
   });
 
-  var shuffle = function(o) {
+  var shuffle = function (o) {
     for (
       var j, x, i = o.length;
       i;
@@ -17,7 +20,7 @@ jQuery("document").ready(function($) {
     return o;
   };
 
-  var shuffleText = function(element, originalText) {
+  var shuffleText = function (element, originalText) {
     var elementTextArray = [];
     var randomText = [];
 
@@ -25,13 +28,13 @@ jQuery("document").ready(function($) {
       elementTextArray.push(originalText.charAt([i]));
     }
 
-    var repeatShuffle = function(times, index) {
+    var repeatShuffle = function (times, index) {
       if (index == times) {
         element.text(originalText);
         return;
       }
 
-      setTimeout(function() {
+      setTimeout(function () {
         randomText = shuffle(elementTextArray);
         for (var i = 0; i < index; i++) {
           randomText[i] = originalText[i];
@@ -45,7 +48,31 @@ jQuery("document").ready(function($) {
     repeatShuffle(element.text().length, 0);
   };
 
-  shuffleElement.mouseenter(function() {
+  shuffleElement.mouseenter(function () {
     shuffleText($(this), $(this).data("text"));
+  });
+});
+
+
+/**
+ * This scroll to top when arrow up is clicked BEGIN
+ */
+$(window).scroll(function () {
+  var height = $(window).scrollTop();
+  if (height > 300) {
+    $('#back2Top').fadeIn();
+  } else {
+    $('#back2Top').fadeOut();
+  }
+});
+
+/**
+ * This scroll to top when arrow up is clicked END
+ */
+$(document).ready(function () {
+  $("#back2Top").click(function (event) {
+    event.preventDefault();
+    $("html, body").animate({ scrollTop: 0 }, "slow");
+    return false;
   });
 });
