b91bce37729edb866d65d15370e668d4c6960d2f
youngua <gyoung@risd.edu>
2020-04-18 23:06:05 -0400

    healthcheck project content update

--- script/jquery.js
+++ script/jquery.js
@@ -1,57 +1,57 @@
 /**
  * This animates the scramble of the navigation text
  */
-jQuery("document").ready(function ($) {
-  // Set effect velocity in ms
-  var velocity = 100;
+// jQuery("document").ready(function ($) {
+//   // Set effect velocity in ms
+//   var velocity = 100;
 
-  var shuffleElement = $(".shuffle");
+//   var shuffleElement = $(".shuffle");
 
-  $.each(shuffleElement, function (index, item) {
-    $(item).attr("data-text", $(item).text());
-  });
+//   $.each(shuffleElement, function (index, item) {
+//     $(item).attr("data-text", $(item).text());
+//   });
 
-  var shuffle = function (o) {
-    for (
-      var j, x, i = o.length;
-      i;
-      j = parseInt(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x
-    );
-    return o;
-  };
+//   var shuffle = function (o) {
+//     for (
+//       var j, x, i = o.length;
+//       i;
+//       j = parseInt(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x
+//     );
+//     return o;
+//   };
 
-  var shuffleText = function (element, originalText) {
-    var elementTextArray = [];
-    var randomText = [];
+//   var shuffleText = function (element, originalText) {
+//     var elementTextArray = [];
+//     var randomText = [];
 
-    for (i = 0; i < originalText.length; i++) {
-      elementTextArray.push(originalText.charAt([i]));
-    }
+//     for (i = 0; i < originalText.length; i++) {
+//       elementTextArray.push(originalText.charAt([i]));
+//     }
 
-    var repeatShuffle = function (times, index) {
-      if (index == times) {
-        element.text(originalText);
-        return;
-      }
+//     var repeatShuffle = function (times, index) {
+//       if (index == times) {
+//         element.text(originalText);
+//         return;
+//       }
 
-      setTimeout(function () {
-        randomText = shuffle(elementTextArray);
-        for (var i = 0; i < index; i++) {
-          randomText[i] = originalText[i];
-        }
-        randomText = randomText.join("");
-        element.text(randomText);
-        index++;
-        repeatShuffle(times, index);
-      }, velocity);
-    };
-    repeatShuffle(element.text().length, 0);
-  };
+//       setTimeout(function () {
+//         randomText = shuffle(elementTextArray);
+//         for (var i = 0; i < index; i++) {
+//           randomText[i] = originalText[i];
+//         }
+//         randomText = randomText.join("");
+//         element.text(randomText);
+//         index++;
+//         repeatShuffle(times, index);
+//       }, velocity);
+//     };
+//     repeatShuffle(element.text().length, 0);
+//   };
 
-  shuffleElement.mouseenter(function () {
-    shuffleText($(this), $(this).data("text"));
-  });
-});
+//   shuffleElement.mouseenter(function () {
+//     shuffleText($(this), $(this).data("text"));
+//   });
+// });
 
 
 /**
