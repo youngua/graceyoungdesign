14165832650d7eaf417d45a238bba599a8098d51
youngua <gyoung@risd.edu>
2023-03-22 18:54:03 +0100

    Mar 22, 2023 Site Update

--- script/script.js
+++ script/script.js
@@ -4,3 +4,11 @@ $(function () {
         $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
     });
 });
+
+function toggle_visibility(visual) {
+    var e = document.getElementById(visual);
+    if (e.style.display == 'flex')
+        e.style.display = 'none';
+    else
+        e.style.display = 'flex';
+}
